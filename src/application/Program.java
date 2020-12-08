0795bc6dbfe9525c6e5f89a762a8ab777395d620
Natalia Tomadon Souza <ntomadon@everis.com>
2020-12-08 17:51:55 -0300

    FindAll implemantation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,9 +20,15 @@ public class Program {
 	
 		System.out.println(seller);
 		
-		System.out.println("\n====== TESTE 2: seller findById ======");
+		System.out.println("\n====== TESTE 2: seller findByDepartment ======");
 		Department department = new Department (2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n====== TESTE 3: seller findAll ======");
+	    list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
